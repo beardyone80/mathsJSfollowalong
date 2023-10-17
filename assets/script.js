6987f259af159e4ae65a4c1e8d88d9000626399b
Paul Beardwell <beardwell@hotmail.com>
2023-10-17 11:35:36 +0000

    added handler for button clicks and calculations handling

--- assets/script.js
+++ assets/script.js
@@ -26,5 +26,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // init the calculation variables
+    let operand1 = "";
+    let operand2 = "";
+    let operator = "";
+
     // handle the button clicks
+    function handleButtonClick(button) {
+        // code for handling button clicks
+        if (button >= "0" && button <= "9") {
+            operand2 += button;
+            display.value = operand2;
+        } else if (["+","-","*","/"].includes(button)) {
+            operator = button;
+            operand1 = operand2;
+            operand2 = "";
+        } else if (button === "C") {
+            operand1 = operand2 = operator = "";
+            display.value = "";
+        } else if (button === "=") {
+            let result = 0;
+            operand1 = parseFloat(operand1);
+            operand2 = parseFloat(operand2);
+            switch (operator) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    result = operand1 / operand2;
+                    break;                    
+            };
+            display.value = result;
+            operand1 = result;
+            operand2 = "";
+        }
+    }
 });
